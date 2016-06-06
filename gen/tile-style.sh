81535262e2ae1608d79b94df8d5e8490882df8c0
James E. Flemer <james.flemer@ndpgroup.com>
2016-06-06 16:16:30 -0600

    Fix style build order

--- gen/tile-style.sh
+++ gen/tile-style.sh
@@ -20,7 +20,7 @@ get_extra_data() {
 }
 
 setup_style() {
-  local url="https://github.com/ndpgroup/osm-bright/archive/5337a8c5bf1764a4dfb7173e02ba147b19b531b4.tar.gz"
+  local url="https://github.com/ndpgroup/osm-bright/archive/ne_boundaries.tar.gz"
   local dir="${TMPDIR:-/tmp}/${STYLE_NAME}"
 
   mkdir -p "${STYLEDIR}/${STYLE_NAME}" "${dir}"
@@ -28,19 +28,19 @@ setup_style() {
   LOG "downloading style from: ${url}"
   wget -O - "${url}" | tar -zxf - --strip=1 -C "${dir}" || return 1
 
+  mkdir -p "${DATADIR}"
+  chmod 1777 "${DATADIR}"
+
+  LOG "downloading & importing natural earth data"
+  su - osm -c "env TMPDIR=${DATADIR} ${dir}/ne2pgsql"
+
+  get_extra_data || return 1
+
   LOG "setting up map style"
   cp /opt/osm/configure.py "${dir}/"
   (cd "${dir}" && ./make.py) || return 1
   (cd "${STYLEDIR}/${STYLE_NAME}" && /opt/osm/node_modules/carto/bin/carto -l -n project.mml > project.xml) || return 1
   cp "${dir}/imposm-mapping.py" "${STYLEDIR}/${STYLE_NAME}/"
-
-  mkdir -p "${DATADIR}"
-  chmod 1777 "${DATADIR}"
-
-  LOG "downloading & importing natural earth data"
-  (cd "${dir}" && su - osm -c "env TMPDIR=${DATADIR} ./ne2pgsql")
-
-  get_extra_data || return 1
 }
 
 export MAPNIK_FONT_PATH=`find /usr/share/fonts -type d | env LC_ALL=C sort | tr '\n' ':'`
